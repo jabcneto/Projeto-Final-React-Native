70e25b1f31420cdfcf149821c37d41222bc0d393
João Barcelos Coutinho <jabcneto@gmail.com>
2020-10-07 14:49:33 -0300

    banco offline funcinando no gerenciar

--- src/pages/Funcionario/index.js
+++ src/pages/Funcionario/index.js
@@ -14,18 +14,20 @@ export default ({navigation}) => {
       const fetchApi = async () => {
         try {
           const res = await api.get('/funcionario');
+          console.log('AQUI');
+
           setFuncionarios(res.data);
+
           Realm.open({schema: [FuncionarioSchema]}).then((realm) => {
             realm.objects('Funcionario');
           });
-        } catch {
-          (e) => {
-            Realm.open({schema: [FuncionarioSchema]}).then((realm) => {
-              console.warn(realm.objects('Funcionario'));
-              setFuncionarios(realm.objects('Funcionario'));
-            });
-            console.log(e);
-          };
+        } catch (e) {
+          console.log('AQUI 2');
+          Realm.open({schema: [FuncionarioSchema]}).then((realm) => {
+            console.warn(realm.objects('Funcionario'));
+            setFuncionarios(realm.objects('Funcionario'));
+          });
+          console.log(e);
         }
       };
       fetchApi();
